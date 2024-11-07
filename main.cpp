85d81a3aad04c0f9c4d6ecdf009502717094aa32
Nick Brown <nworbn@gmail.com>
2024-11-06 16:05:53 -0800

    changing to atmega328

---
+++ main.cpp
@@ -0,0 +1,175 @@
+#include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
+#include <SPI.h>
+
+#define NUM_ROWS 16
+#define NUM_COLS 16
+#define NUM_LEDS ROWS *COLS
+#define NUM_BLANK_CYCLES 0
+
+#define EXT_DISPLAY_PIN 12 // external display pin
+#define OE_PIN 1           // !OE, can be tied to GND if need to save pin
+#define LATCH_PIN 0        // RCLK
+// clock and data pins are defined in SPI library
+
+#define RGB_LED_BRIGHTNESS 1
+#define RGB_LED_PIN 2
+#define RGB_LED_COUNT 3
+
+bool statusLed = false;
+uint32_t Yellow = Adafruit_NeoPixel::Color(6 * RGB_LED_BRIGHTNESS, 3 * RGB_LED_BRIGHTNESS, 0);
+uint32_t Amber = Adafruit_NeoPixel::Color(6 * RGB_LED_BRIGHTNESS, 2 * RGB_LED_BRIGHTNESS, 0);
+uint32_t Orange = Adafruit_NeoPixel::Color(6 * RGB_LED_BRIGHTNESS, 1 * RGB_LED_BRIGHTNESS, 0);
+uint32_t Red = Adafruit_NeoPixel::Color(6 * RGB_LED_BRIGHTNESS, 0, 0);
+uint32_t Colors[] = {Yellow, Amber, Orange, Red};
+Adafruit_NeoPixel rgbLeds(RGB_LED_COUNT, RGB_LED_PIN, NEO_GRB + NEO_KHZ800);
+
+volatile bool display = true;
+volatile uint16_t displayBuffer[NUM_ROWS] = {
+    (uint16_t)0b0000000000000000,
+    (uint16_t)0b0000000110000000,
+    (uint16_t)0b0000001111000000,
+    (uint16_t)0b0000001111000000, 
+    (uint16_t)0b0111111111111110,
+    (uint16_t)0b0011110110111100,
+    (uint16_t)0b0001110110111000,
+    (uint16_t)0b0000111111110000,
+    (uint16_t)0b0000111111110000,
+    (uint16_t)0b0000111111110000,
+    (uint16_t)0b0001111111111000,
+    (uint16_t)0b0001111111111000,
+    (uint16_t)0b0011111001111100,
+    (uint16_t)0b0011100000011100,
+    (uint16_t)0b0110000000000110,
+    (uint16_t)0b0000000000000000,
+};
+
+uint16_t limiter = 0;
+volatile int curLine = 0;
+volatile int blankCycles = 0; // between each line cycle
+
+ISR(TIMER2_COMPA_vect)
+{ // timer1 interrupt
+  // if (limiter++ < 2)
+  //    return;
+  // limiter = 0;
+
+  uint16_t rowData = (blankCycles > 0 || !display) ? 0xFFFF : ~displayBuffer[curLine];
+  uint16_t rowSelect = (blankCycles > 0 || !display) ? 0xFFFF : ~(0x01 << curLine);
+  SPI.transfer16(rowData);
+  SPI.transfer16(rowSelect);
+  digitalWrite(LATCH_PIN, LOW);
+  digitalWrite(LATCH_PIN, HIGH);
+
+  blankCycles--;
+
+  if (blankCycles < 0)
+  {
+    curLine = (curLine + 1) % NUM_ROWS;
+    // curLine--;
+    // if (curLine < 0) {
+    //   curLine = NUM_ROWS - 1;
+    // }
+    blankCycles = 2;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Starting setup...");
+
+  pinMode(EXT_DISPLAY_PIN, INPUT_PULLUP);
+  pinMode(OE_PIN, OUTPUT);
+  pinMode(LATCH_PIN, OUTPUT);
+
+  digitalWrite(OE_PIN, !display);
+  digitalWrite(LATCH_PIN, LOW);
+
+  SPI.begin();
+
+  cli();
+
+  // // set timer0 for interrupt at REFRESH_RATE_US us interval or REFRESH_RATE_US Hz
+  // TCCR0A = TCCR0B = TCNT0 = 0;
+  // OCR0A = 61; // 4kHz /w 64 prescaler
+  // TCCR0A |= (1 << WGM01);
+  // TCCR0B |= (1 << CS01) | (1 << CS00);
+  // TIMSK0 |= (1 << OCIE0A);
+
+  // set timer2 interrupt at 8kHz
+  TCCR2A = 0; // set entire TCCR2A register to 0
+  TCCR2B = 0; // same for TCCR2B
+  TCNT2 = 0;  // initialize counter value to 0
+  // set compare match register for 8khz increments
+  OCR2A = 249; // = (16*10^6) / (8000*8) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR2A |= (1 << WGM21);
+  // Set CS21 bit for 8 prescaler
+  TCCR2B |= (1 << CS21);
+  // enable timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A);
+
+  sei();
+
+  rgbLeds.begin();
+
+  Serial.println("Finished setup");
+}
+
+int start = 0;
+void loop()
+{
+  Serial.println(start++);
+  rgbLeds.clear();
+
+  display = digitalRead(EXT_DISPLAY_PIN);
+  digitalWrite(OE_PIN, !display);
+
+  if (display)
+  {
+    for (int i = 0; i < RGB_LED_COUNT; i++)
+    {
+      rgbLeds.setPixelColor(i, Colors[(i + start) % 3]);
+    }
+  }
+  rgbLeds.show();
+
+  start++;
+  delay(400);
+  // scan animation test
+
+  // // row scan
+  // for (int row = 0; row < NUM_ROWS; row++) {
+  //   for (int y = 0; y < NUM_ROWS; y++) {
+  //     for (int x = 0; x < NUM_COLS; x++) {
+  //       setPixel(x, y, y == row);
+  //     }
+  //   }
+  //   delay(250);
+  // }
+
+  // // col scan
+  // for (int col = 0; col < NUM_COLS; col++) {
+  //   for (int x = 0; x < NUM_COLS; x++) {
+  //     for (int y = 0; y < NUM_ROWS; y++) {
+  //       setPixel(x, y, x == col);
+  //     }
+  //   }
+  //   delay(250);
+  // }
+
+  // // diagonal scan
+  // for (int diag = 0; diag < NUM_ROWS + NUM_COLS - 1; diag++) {
+  //   for (int x = 0; x < NUM_COLS; x++) {
+  //     for (int y = 0; y < NUM_ROWS; y++) {
+  //       if (x + y == diag) {
+  //         setPixel(x, y, 1);
+  //       } else {
+  //         setPixel(x, y, 0);
+  //       }
+  //     }
+  //   }
+  //   delay(250);
+  // }
+}
--- services.hpp
+++ services.hpp
@@ -1,174 +1,174 @@
-#include <ArduinoOTA.h>
-#include <ESPmDNS.h>
-#include <WebServer.h>
-#include <WiFi.h>
+// #include <ArduinoOTA.h>
+// #include <ESPmDNS.h>
+// #include <WebServer.h>
+// #include <WiFi.h>
 
-#include "secrets.h"
+// #include "secrets.h"
 
-WebServer restServer(80);
-extern volatile bool display;
-extern char *matrix5x5Message;
-extern char *matrix13x9Message;
-extern char *matrix16x9Message;
+// WebServer restServer(80);
+// extern volatile bool display;
+// extern char *matrix5x5Message;
+// extern char *matrix13x9Message;
+// extern char *matrix16x9Message;
 
-void wifiSetup()
-{
-  Serial.println("Wifi setting up...");
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  WiFi.setTxPower(WIFI_POWER_8_5dBm); // seems to improve connection stability when issues on default tx power
-  Serial.print("Wifi TX: ");
-  Serial.println(WiFi.getTxPower());
-
-  while (WiFi.waitForConnectResult() != WL_CONNECTED)
-  {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-
-  Serial.print("Wifi ready, IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-volatile unsigned long wifiLastCheckMillis = 0;
-volatile int wifiStatusDelayMs = 5000;
-int wifiDisconnects = 0;
-void checkWifiStatus()
-{
-  if (millis() > wifiLastCheckMillis + wifiStatusDelayMs)
-  {
-    log_v("Wifi TX: ");
-    log_v(WiFi.getTxPower());
-
-    try
-    {
-      if (WiFi.status() != WL_CONNECTED)
-      {
-        log_w("Reconnecting to WiFi...");
-        WiFi.disconnect();
-        WiFi.reconnect();
-        wifiDisconnects++;
-        log_w("Reconnected to WiFi");
-      }
-    }
-    catch (const std::exception &e)
-    {
-      log_e("Wifi error: %s", String(e.what()));
-      wifiStatusDelayMs = 10 * 60 * 1000; // 10 minutes
-    }
-
-    wifiLastCheckMillis = millis();
-    wifiStatusDelayMs = 60 * 1000; // 1 minute
-  }
-}
-
-void mDnsSetup()
-{
-  if (!MDNS.begin("led-scan-controller"))
-  {
-    log_e("Error setting up MDNS responder!");
-    return;
-  }
-
-  log_d("mDNS responder started");
-}
-
-void otaSetup()
-{
-  Serial.println("OTA setting up...");
-
-  ArduinoOTA.setHostname("LED-Scan-Controller");
-
-  ArduinoOTA
-      .onStart([]()
-               {
-        String type;
-        if (ArduinoOTA.getCommand() == U_FLASH)
-          type = "sketch";
-        else  // U_SPIFFS
-          type = "filesystem";
-
-        Serial.println("Start updating " + type); })
-      .onEnd([]()
-             { Serial.println("\nEnd"); })
-      .onProgress([](unsigned int progress, unsigned int total)
-                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
-      .onError([](ota_error_t error)
-               {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR)
-          Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR)
-          Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR)
-          Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR)
-          Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR)
-          Serial.println("End Failed"); });
-  ArduinoOTA.begin();
-
-  Serial.println("OTA setup complete.");
-}
-
-void restIndex()
-{
-  Serial.println("Serving index.html");
-  restServer.send(200, "text/plain", "test");
-  Serial.println("Served index.html");
-}
-
-void restDisplay()
-{
-  if (restServer.hasArg("plain"))
-  {
-    String body = restServer.arg("plain");
-    body.toLowerCase();
-
-    if (body == "off")
-    {
-      Serial.println("Turning display off");
-      display = false;
-    }
-    else if (body == "on")
-    {
-      Serial.println("Turning display on");
-      display = true;
-    }
-    else
-    {
-      restServer.send(400, "text/plain", body);
-      return;
-    }
-  }
-
-  restServer.send(200, "text/plain", display ? "on" : "off");
-}
-
-// void restSetMessage()
+// void wifiSetup()
 // {
-//   if (!restServer.hasArg("message"))
+//   Serial.println("Wifi setting up...");
+
+//   WiFi.mode(WIFI_STA);
+//   WiFi.begin(WIFI_SSID, WIFI_PASS);
+//   WiFi.setTxPower(WIFI_POWER_8_5dBm); // seems to improve connection stability when issues on default tx power
+//   Serial.print("Wifi TX: ");
+//   Serial.println(WiFi.getTxPower());
+
+//   while (WiFi.waitForConnectResult() != WL_CONNECTED)
 //   {
-//     restServer.send(400, "text/plain", "No message provided");
-//     log_w("No message provided");
+//     Serial.println("Connection Failed! Rebooting...");
+//     delay(5000);
+//     ESP.restart();
+//   }
+
+//   Serial.print("Wifi ready, IP address: ");
+//   Serial.println(WiFi.localIP());
+// }
+
+// volatile unsigned long wifiLastCheckMillis = 0;
+// volatile int wifiStatusDelayMs = 5000;
+// int wifiDisconnects = 0;
+// void checkWifiStatus()
+// {
+//   if (millis() > wifiLastCheckMillis + wifiStatusDelayMs)
+//   {
+//     log_v("Wifi TX: ");
+//     log_v(WiFi.getTxPower());
+
+//     try
+//     {
+//       if (WiFi.status() != WL_CONNECTED)
+//       {
+//         log_w("Reconnecting to WiFi...");
+//         WiFi.disconnect();
+//         WiFi.reconnect();
+//         wifiDisconnects++;
+//         log_w("Reconnected to WiFi");
+//       }
+//     }
+//     catch (const std::exception &e)
+//     {
+//       log_e("Wifi error: %s", String(e.what()));
+//       wifiStatusDelayMs = 10 * 60 * 1000; // 10 minutes
+//     }
+
+//     wifiLastCheckMillis = millis();
+//     wifiStatusDelayMs = 60 * 1000; // 1 minute
+//   }
+// }
+
+// void mDnsSetup()
+// {
+//   if (!MDNS.begin("led-scan-controller"))
+//   {
+//     log_e("Error setting up MDNS responder!");
 //     return;
 //   }
 
-//   String newMessage = restServer.arg("message");
-//   newMessage = newMessage.substring(0, 100);
-//   strcpy(curMessage, newMessage.c_str());
-
-//   restServer.send(200, "text/plain", curMessage);
+//   log_d("mDNS responder started");
 // }
 
-void restSetup()
-{
-  restServer.on("/", HTTP_GET, restIndex);
-  restServer.on("/display", restDisplay);
-//   restServer.on("/16x16", HTTP_GET, restSetMessage);
-  restServer.begin();
+// void otaSetup()
+// {
+//   Serial.println("OTA setting up...");
 
-  log_d("REST server running");
-}
+//   ArduinoOTA.setHostname("LED-Scan-Controller");
+
+//   ArduinoOTA
+//       .onStart([]()
+//                {
+//         String type;
+//         if (ArduinoOTA.getCommand() == U_FLASH)
+//           type = "sketch";
+//         else  // U_SPIFFS
+//           type = "filesystem";
+
+//         Serial.println("Start updating " + type); })
+//       .onEnd([]()
+//              { Serial.println("\nEnd"); })
+//       .onProgress([](unsigned int progress, unsigned int total)
+//                   { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+//       .onError([](ota_error_t error)
+//                {
+//         Serial.printf("Error[%u]: ", error);
+//         if (error == OTA_AUTH_ERROR)
+//           Serial.println("Auth Failed");
+//         else if (error == OTA_BEGIN_ERROR)
+//           Serial.println("Begin Failed");
+//         else if (error == OTA_CONNECT_ERROR)
+//           Serial.println("Connect Failed");
+//         else if (error == OTA_RECEIVE_ERROR)
+//           Serial.println("Receive Failed");
+//         else if (error == OTA_END_ERROR)
+//           Serial.println("End Failed"); });
+//   ArduinoOTA.begin();
+
+//   Serial.println("OTA setup complete.");
+// }
+
+// void restIndex()
+// {
+//   Serial.println("Serving index.html");
+//   restServer.send(200, "text/plain", "test");
+//   Serial.println("Served index.html");
+// }
+
+// void restDisplay()
+// {
+//   if (restServer.hasArg("plain"))
+//   {
+//     String body = restServer.arg("plain");
+//     body.toLowerCase();
+
+//     if (body == "off")
+//     {
+//       Serial.println("Turning display off");
+//       display = false;
+//     }
+//     else if (body == "on")
+//     {
+//       Serial.println("Turning display on");
+//       display = true;
+//     }
+//     else
+//     {
+//       restServer.send(400, "text/plain", body);
+//       return;
+//     }
+//   }
+
+//   restServer.send(200, "text/plain", display ? "on" : "off");
+// }
+
+// // void restSetMessage()
+// // {
+// //   if (!restServer.hasArg("message"))
+// //   {
+// //     restServer.send(400, "text/plain", "No message provided");
+// //     log_w("No message provided");
+// //     return;
+// //   }
+
+// //   String newMessage = restServer.arg("message");
+// //   newMessage = newMessage.substring(0, 100);
+// //   strcpy(curMessage, newMessage.c_str());
+
+// //   restServer.send(200, "text/plain", curMessage);
+// // }
+
+// void restSetup()
+// {
+//   restServer.on("/", HTTP_GET, restIndex);
+//   restServer.on("/display", restDisplay);
+// //   restServer.on("/16x16", HTTP_GET, restSetMessage);
+//   restServer.begin();
+
+//   log_d("REST server running");
+// }
